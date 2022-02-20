c37c7902b86818d2b4434c16fa53264f1a5f1dfd
Srikanth <sri1@publicisgroupe.net>
2022-02-20 12:45:22 +0530

    switch programm in added in c

--- 9-switch/index.c
+++ 9-switch/index.c
@@ -1,7 +1,48 @@
 #include<stdio.h>
 
+/*
+    The break statement breaks out of the switch block and stops the execution
+    The default statement is optional, and specifies some code to run if there is no case match
+*/
+
 int main(){
 
-    printf("ehyy");
+    int currentDayInNumber = 10;
+
+    switch (currentDayInNumber)
+    {
+    case 1:
+        printf("Heyy, Monday");
+        break;
+
+    case 2:
+        printf("Heyy, Tuesday");
+        break;
+
+    case 3:
+        printf("Heyy, Wednesday");
+        break;
+
+    case 4:
+        printf("Heyy, Thursday");
+        break;
+
+    case 5:
+        printf("Heyy, Friday");
+        break;
+
+    case 6:
+        printf("Heyy, Saturday");
+        break;
+
+    case 7:
+        printf("Heyy, Sunday");
+        break;
+    
+    default:
+        printf("Please enter a proper day number!!!)--");
+        break;
+    }
+
     return 0;
 }
