13cdd1c00d99b41e5b989e061faa3c1e31dcdb90
Srikanth <sri1@publicisgroupe.net>
2022-02-21 15:23:30 +0530

    format specifiers && escape characters list added

--- 2-output/output.c
+++ 2-output/output.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+/*
+  Escape Sequences in C (NO NEED TO REMEMBER ALL, but Just Look)
+
+  \a  =>  alarm || beep sound
+  \b  =>  backspace
+  \f  =>  form feed
+  \n  =>  new line || nex line || enter
+  \r  =>  carriage return
+  \t  =>  tab space (Horizontal)
+  \v  =>  vertical tab
+  \\  =>  backslash
+  \'  =>  single quote
+  \"  =>  double quote
+  \?  =>  question mark
+  \ooo  =>  octal number
+  \xhh  =>  hexa decimal number
+  \0  =>  null
+  
+*/
+
 int main() {
   printf("Hello World!\n");
   printf("I am learning\tC");
--- 4-variables/index.c
+++ 4-variables/index.c
@@ -7,6 +7,30 @@
     for int => %d
     for float => %f
     for char => %c
+
+    LIST OF FORMAT SPECIFIERS (NO NEED TO REMEMBER ALL - JUST LOOK and UNDERSTAND)
+    %c                  =>  Character
+    %d                  =>  Signed integer
+    %e or %E            =>  Scientific notation of floats
+    %f                  =>  Float values
+    %g or %G            =>  Similar as %e or %E
+    %hi                 =>  Signed integer (short)
+    %hu                 =>  Unsigned Integer (short)
+    %i                  =>  Unsigned integer
+    %l or %ld or %li    =>  Long
+    %lf                 =>  Double
+    %Lf                 =>  Long double
+    %lu                 =>  Unsigned int or unsigned long
+    %lli or %lld        =>  Long long
+    %llu                =>  Unsigned long long
+    %o                  =>  Octal representation
+    %p                  =>  Pointer
+    %s                  =>  String
+    %u                  =>  Unsigned int
+    %x or %X            =>  Hexadecimal representation
+    %n                  =>  Prints nothing
+    %%                  =>  Prints % character
+    
 */
 
 /*
